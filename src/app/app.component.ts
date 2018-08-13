0fbd4f54975809700d65c79593fc3b7ee740cf38
DaMundl <lukas.schuetzenhofer@gmx.at>
2018-08-13 16:12:24 +0200

    finsished chapter 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
+import { FormControl } from '@angular/forms';
 import { JumbotronComponent } from './jumbotron.component';
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
   template:
   `
-  <app-login></app-login>
+  <input class="form-control" type="search" [formControl]="searchControl">
+
   `
 })
 export class AppComponent {
-  title = 'Fake Amazon';
+  searchControl = new FormControl();
+
+  constructor() {
+    this.searchControl.valueChanges
+    .pipe(filter(text => text.length >= 3))
+    .pipe(debounceTime(400))
+    .pipe(distinctUntilChanged())
+    .subscribe( value => { console.log(value);
+    });
+  }
+
 
 }
